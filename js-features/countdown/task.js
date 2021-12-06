d36132f2812b8f7d15ae5e2d551e973c740d4b79
Igor Suvorov <2195183@mail.ru>
2021-12-06 22:51:41 +0500

    Completed homeworks Countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,3 +1,32 @@
-function first() {
-    alert('Первое сообщение!')
+// Таймер обратного отсчёта
+function startTimer() {
+  const elementMain = document.getElementById('timer');
+
+  const element = setInterval(() => {
+    elementMain.textContent --;
+    if (elementMain.textContent === '0') {
+      clearInterval(element);
+      alert('Вы победили в конкурсе!')
+    }
+  }, 1000);
 }
+
+startTimer();
+
+
+// Повышенный уровень сложности #1
+/*
+const outTimer = document.getElementById('timer');
+
+const element = setInterval(() => {
+  let hour = new Date().getHours();
+  let minute = new Date().getMinutes();
+  let sec = new Date().getSeconds();
+
+  if (hour < 10) hour = '0' + hour;
+  if (minute < 10) minute = '0' + minute;
+  if (sec < 10) sec = '0' + sec;
+  
+  outTimer.innerHTML = '' + hour + ':' + minute + ':' + sec + '';  
+}, 1000);
+*/
