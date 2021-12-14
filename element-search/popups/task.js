a1c161b03216cc0b194608b337b8b917e8dede82
Igor Suvorov <2195183@mail.ru>
2021-12-15 00:41:59 +0500

    Completed homework Popups_2

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,11 +1,15 @@
 const modal = document.getElementsByClassName('modal');
-const modalClose = document.getElementsByClassName('modal__close');
+const modalClose = Array.from(document.getElementsByClassName('modal__close_times'));
 const btnDanger = document.querySelector('.btn_danger');
 
 modal[0].classList.add('modal_active');
-[...modalClose].map(item => item.onclick = () => [...modal].map(item => item.classList.remove('modal_active')));
 
 btnDanger.onclick = () => {
   modal[0].classList.remove('modal_active');
   modal[1].classList.add('modal_active');
 }
+
+modalClose.forEach(elem => elem.onclick = function () {
+  this.closest('.modal').classList.remove('modal_active')
+});
+
