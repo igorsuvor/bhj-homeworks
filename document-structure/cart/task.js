a95dc8e05f6fbfe0b32f3cfca0c732178926546f
Igor Suvorov <2195183@mail.ru>
2022-01-07 00:29:48 +0500

    Completed homework Cart

---
+++ document-structure/cart/task.js
@@ -0,0 +1,43 @@
+const minus = [...document.querySelectorAll('.product__quantity-control_dec')];
+const plus = [...document.querySelectorAll('.product__quantity-control_inc')];
+const productAdd = [...document.querySelectorAll('.product__add')];
+const cartProducts = document.querySelector('.cart__products');
+
+minus.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    elem.nextElementSibling.innerHTML--;
+      if(elem.nextElementSibling.innerHTML < 1) {
+        elem.nextElementSibling.innerHTML = 1;
+      }
+  });
+});
+
+plus.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    elem.previousElementSibling.innerHTML++;
+  });
+});
+
+productAdd.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    const productID = elem.closest('.product').dataset.id;
+    const cartProduct = [...document.querySelectorAll('.cart__product')];
+    const productImg = elem.closest('.product').querySelector('img').src;
+    const productQuantity = elem.closest('.product').querySelector('.product__quantity-value');
+
+    cartProduct.find(elem => productID == elem.dataset.id);
+    if (cartProduct.find(elem => productID == elem.dataset.id) == undefined) {
+
+      cartProducts.insertAdjacentHTML('beforeend',
+        `<div class="cart__product" data-id="${productID}">
+          <img class="cart__product-image" src ="${productImg}">
+          <div class="cart__product-count">${productQuantity.innerHTML}</div>
+        </div>`
+        )
+    } else {
+        const addElement = cartProduct.find(elem => productID == elem.dataset.id);
+        const addValue = addElement.lastElementChild;
+        addValue.innerHTML = +addValue.innerHTML + +productQuantity.innerHTML;
+    }
+  });
+});
