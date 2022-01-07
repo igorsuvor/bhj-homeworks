c54557761aecc21fd8c72ab144897bee9cf9bd9c
Igor Suvorov <2195183@mail.ru>
2022-01-07 22:42:04 +0500

    Completed homework Tooltip_2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,8 +3,14 @@ const divTooltip = document.createElement('div');
 
 hasTooltip.forEach((elem) => {
   elem.addEventListener('click', () => {
-    divTooltip.className = 'tooltip tooltip_active';
-    divTooltip.setAttribute('style', `left: ${elem.getBoundingClientRect().left}px; top: ${elem.getBoundingClientRect().bottom}px`);
+    divTooltip.classList.add('tooltip');
+    divTooltip.classList.toggle('tooltip_active');
+
+    divTooltip.setAttribute('style', 
+    `left: ${elem.getBoundingClientRect().left}px; 
+    top: ${elem.getBoundingClientRect().bottom}px`
+    );
+
     divTooltip.textContent = `${elem.title}`;
     elem.insertAdjacentElement('afterend', divTooltip);
     event.preventDefault();
